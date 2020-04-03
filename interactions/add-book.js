744163ef54414c459fbbe822201c9582192e3c37
Rasha ali <rashaali84@gmail.com>
2020-04-03 14:52:39 +0200

    change query selector

--- interactions/add-book.js
+++ interactions/add-book.js
@@ -3,10 +3,17 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
     // Prevent actual submit
     e.preventDefault();
 
-    // Get form values
-    const title = document.querySelector('#title').value;
-    const author = document.querySelector('#author').value;
-    const isbn = document.querySelector('#isbn').value;
+    // Get form values there two ways to get the data from the form object
+    //way 1 
+    /* const title = document.querySelector('#title').value;
+     const author = document.querySelector('#author').value;
+     const isbn = document.querySelector('#isbn').value;*/
+    //way 2  using new FormDAta(and has the target of the event which is the form 
+    //to be submitted )
+    const formData = new FormData(e.target);
+    const title = formData.get('title');
+    const author = formData.get('author');
+    const isbn = formData.get('isbn');
 
     // Validate
     if (title === '' || author === '' || isbn === '') {
